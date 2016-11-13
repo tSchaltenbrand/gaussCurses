48bcabef4ebdc81958c66a8b56d4b8a86122e221
Carcha <tschaltenbrand@tfwno.gf>
2016-11-12 23:49:34 -0500

    clean up, change to using rationals, and fix the matrix not changing size correctly

--- matrix.py
+++ matrix.py
@@ -1,9 +1,11 @@
 class Matrix:
     """Simple matrix class supporting matrix addition and multiplication"""
-    def __init__(self):
-        self.__rows = 0
+    def __init__(self, m = [[]]):
+        self.__matrix = m
+        self.__rows = len(m)
         self.__cols = 0
-        self.__matrix = []
+        if self.__rows > 0:
+            self.__cols = len(m[0])
 
     def setRows(self, n):
         if n > self.__rows:
@@ -19,8 +21,8 @@ class Matrix:
             for col in self.__matrix:
                 col += [0 for i in range(n - self.__cols)]
         else:
-            for col in self.__matrix:
-                col = col[0:(n - self.__cols)]
+            for i in range(self.__rows):
+                self.__matrix[i] = self.__matrix[i][0:(n - self.__cols)]
         self.__cols = n
     def getCols(self):
         return self.__cols
@@ -43,14 +45,24 @@ class Matrix:
                     sumMat.setValue(i,j,self.__matrix[j][i] + other.getValue(i,j))
         return sumMat
     def __mul__(self, other):
-        mulMat = Matrix()
+        mulMat = Matrix([[0 for i in range(other.getCols())] for j in range(self.__rows)])
         if self.__cols == other.getRows():
-            mulMat.setRows(self.__rows)
-            mulMat.setCols(other.getCols())
             for j in range(mulMat.getRows()):
                 for i in range(mulMat.getCols()):
                     val = 0
                     for n in range(self.__cols):
                         val += self.__matrix[j][n] * other.getValue(i,n)
+                    print(i,j,mulMat.getMatrix())
                     mulMat.setValue(i,j,val)
         return mulMat
+    def __repr__(self):
+        maxLotSize = max([max([len(str(i)) for i in row]) for row in self.__matrix])
+        outString = '┌' + ' ' * (1 + self.__cols * (1 + maxLotSize)) + '┐' + '\n'
+        for row in self.__matrix:
+            outString += "│ " + ' '.join([format(str(i), ">" + str(maxLotSize) +"s") for i in row]) + " │" + '\n'
+        outString += '└' + ' ' * (1 + self.__cols * (1 + maxLotSize)) + '┘'
+        return outString
+
+def identity(n, one = 1, zero = 0):
+    """Returns an n * n identity matrix"""
+    return Matrix([[one if i == j else zero for i in range(n)] for j in range(n)])
